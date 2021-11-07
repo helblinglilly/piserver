86596431ea56230a222e458c119a4f3918f23358
Joel Helbling <helblingjoel@gmail.com>
2021-11-07 01:28:12 +0000

    Adds Bulma Partially implements /ac/fish?name

--- controllers/ac.controller.js
+++ controllers/ac.controller.js
@@ -1,5 +1,17 @@
 const utils = require("../utils");
+const acModel = require("../models/ac.model");
 
 exports.getRoot = (_, res, next) => {
   res.sendFile(utils.views() + "ac/index.html");
 };
+
+exports.getFish = (req, res, next) => {
+  if (req.query.name === undefined)
+    res.sendFile(utils.views() + "ac/fish.html");
+  else {
+    acModel.GetFishDetails(req.query.name);
+  }
+  // http://acnhapi.com/v1/fish/{fishID}
+};
+
+exports.getFishById = (req, res, next) => {};
--- routers/ac.router.js
+++ routers/ac.router.js
@@ -2,5 +2,6 @@ const acRouter = require("express").Router();
 const acController = require("../controllers/ac.controller");
 
 acRouter.get("/", acController.getRoot);
+acRouter.get("/fish", acController.getFish);
 
 module.exports = acRouter;
