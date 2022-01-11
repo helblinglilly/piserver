d41d5b01bc5b4236c07c939ce3a9426147e9bf88
Joel Helbling <helblingjoel@gmail.com>
2022-01-11 19:28:37 +0000

    Did the maths

--- controllers/timesheets.controller.js
+++ controllers/timesheets.controller.js
@@ -6,7 +6,7 @@ exports.getTimesheets = async (req, res, next) => {
   options.day = utils.today();
 
   const ip = req.socket.remoteAddress;
-  const username = await timesheetsModel.getUsername(ip);
+  const username = await timesheetsModel.selectUsername(ip);
 
   const rows = await timesheetsModel.selectDay(utils.todayIso(), username);
 
@@ -17,7 +17,7 @@ exports.getTimesheets = async (req, res, next) => {
       hours: 8,
       minutes: 30,
     });
-    options.endTime = `${endTime.getHours()}:${endTime.getMinutes()}`;
+    options.endTime = `${utils.DateTimeToTime(endTime)}`;
   } else {
     if (!rows.break_in && !rows.clock_out) {
       options.nextAction = "Break In";
@@ -27,16 +27,31 @@ exports.getTimesheets = async (req, res, next) => {
       clockIn.setMinutes(rows.clock_in.substr(3, 2));
 
       let endTime = utils.addTime(clockIn, { hours: 8, minutes: 30 });
-      options.endTime = `${endTime.getHours()}:${endTime.getMinutes()}`;
+      options.endTime = `${utils.DateTimeToTime(endTime)}`;
     } else if (rows.break_in && !rows.break_out && !rows.clock_out) {
       options.nextAction = "Break Out";
 
-      const clockIn = new Date();
-      clockIn.setHours(rows.clock_in.substr(0, 2));
-      clockIn.setMinutes(rows.clock_in.substr(3, 2));
+      const clock_in = new Date(rows.day_date);
+      clock_in.setHours(rows.clock_in.substr(0, 2));
+      clock_in.setMinutes(rows.clock_in.substr(3, 2));
 
-      let endTime = utils.addTime(clockIn, { hours: 8, minutes: 30 });
-      options.endTime = `${endTime.getHours()}:${endTime.getMinutes()}`;
+      const break_in = new Date(rows.day_date);
+      break_in.setHours(rows.break_in.substr(0, 2));
+      break_in.setMinutes(rows.break_in.substr(3, 2));
+
+      const backFromBreak = utils.addTime(break_in, { hours: 1, minutes: 0 });
+      const backFromBreakTime = utils.DateTimeToTime(backFromBreak);
+
+      const millisecondsLeft = 7.5 * 60 * 60 * 1000 - (break_in - clock_in); // 7.5hours/day work contract
+      const timeLeftAtWork = {
+        hours: Math.round(millisecondsLeft / 60 / 60 / 1000),
+        minutes: ((millisecondsLeft / 60 / 60 / 1000) % 1) * 60,
+      };
+      const doneWithWork = utils.DateTimeToTime(
+        utils.addTime(backFromBreak, timeLeftAtWork)
+      );
+
+      options.endTime = `Back: ${backFromBreakTime}\nDone by: ${doneWithWork}`;
     }
   }
 
@@ -63,6 +78,18 @@ exports.enter = (req, res, next) => {
   } else {
     next({ statusCode: 400, msg: "Page not found" });
   }
+};
+
+exports.selectGet = (_, res, next) => {
+  res.render("timesheets/select");
+};
+
+exports.selectPost = (req, res, next) => {
+  const ip = req.socket.remoteAddress;
+  const username = req.body.username;
+  timesheetsModel.insertUsertable(ip, username);
+  console.log("done");
+  res.send(`Hello ${req.body.username}`);
 };
 
 exports.view = (req, res, next) => {
