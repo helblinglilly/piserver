705d37fd52bae7fa709e094637d20a1bd5b3183d
Joel Helbling <helblingjoel@gmail.com>
2022-01-21 23:01:37 +0000

    Completed Edit UI

---
+++ public/scripts/timesheet_edit.js
@@ -0,0 +1,66 @@
+function edit(action) {
+  let editLabel;
+  let saveLabel;
+  let input;
+
+  if (action === "clock_in") {
+    editLabel = document.getElementById("a-edit-clock_in");
+    saveLabel = document.getElementById("a-save-clock_in");
+    input = document.getElementById("input-clock_in");
+  } else if (action === "break_in") {
+    editLabel = document.getElementById("a-edit-break_in");
+    saveLabel = document.getElementById("a-save-break_in");
+    input = document.getElementById("input-break_in");
+  } else if (action === "break_out") {
+    editLabel = document.getElementById("a-edit-break_out");
+    saveLabel = document.getElementById("a-save-break_out");
+    input = document.getElementById("input-break_out");
+  } else if (action === "clock_out") {
+    editLabel = document.getElementById("a-edit-clock_out");
+    saveLabel = document.getElementById("a-save-clock_out");
+    input = document.getElementById("input-clock_out");
+  }
+
+  toggleEditLabel(editLabel);
+  toggleSaveLabel(saveLabel);
+  toggleInput(input);
+}
+
+function toggleEditLabel(editLabel) {
+  if (editLabel.innerHTML === "Edit") {
+    editLabel.innerHTML = "Lock";
+  } else {
+    editLabel.innerHTML = "Edit";
+  }
+}
+
+function toggleSaveLabel(saveLabel) {
+  if (saveLabel.getAttribute("enabled") === "false") {
+    saveLabel.setAttribute("enabled", "true");
+    saveLabel.removeAttribute("style");
+  } else {
+    saveLabel.setAttribute("enabled", "false");
+    saveLabel.setAttribute("style", "color: #c2c2c2;");
+  }
+}
+
+function toggleInput(input) {
+  if (input.classList.contains("is-danger")) {
+    input.removeAttribute("readonly");
+    input.classList.remove("is-danger");
+    input.classList.add("is-success");
+    input.removeAttribute("style");
+  } else {
+    input.setAttribute("readonly", true);
+    input.classList.add("is-danger");
+    input.classList.remove("is-success");
+    input.setAttribute("style", "color:#c2c2c2");
+  }
+}
+function save(action) {
+  if (action === "clock_in") {
+    let state = document.getElementById("a-save-clock_in");
+    state = state.getAttribute("enabled");
+    if (state === "true") document.forms["save-clock_in"].submit();
+  }
+}
