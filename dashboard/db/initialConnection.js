7645e0f152ed17469fa5c8d5586604e6a561b60f
Joel Helbling <helblingjoel@gmail.com>
2022-09-10 22:20:21 +0100

    Validate all env variables are present

--- dashboard/db/initialConnection.js
+++ dashboard/db/initialConnection.js
@@ -3,8 +3,31 @@ const env = require("../environment");
 
 require("dotenv").config();
 
+OCTOPUS_ELECTRIC_MPAN = 2340313769017;
+const requiredEnvVars = [
+  "POSTGRES_USER",
+  "POSTGRES_HOST",
+  "POSTGRES_DATABASE",
+  "POSTGRES_PASSWORD",
+  "OCTOPUS_ACCOUNT_NUMBER",
+  "OCTOPUS_API_KEY",
+  "OCTOPUS_ELECTRIC_MPAN",
+  "OCTOPUS_ELECTRIC_SERIAL",
+  "OCTOPUS_GAS_MPRN",
+  "OCTOPUS_GAS_SERIAL",
+  "MOVE_IN_DATE",
+  "ADDRESS_CODE",
+];
+
 module.exports.initialise = () => {
   return new Promise(async (resolve, reject) => {
+    requiredEnvVars.forEach((variable) => {
+      if (process.env[variable] === undefined) {
+        console.log(`Missing environment variable ${variable}! Shutting down...`);
+        process.exit(0);
+      }
+    });
+
     try {
       const dbClient = new Client({
         user: process.env.POSTGRES_USER,
