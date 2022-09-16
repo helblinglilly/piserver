a24bf4b89ccc58860cde2faa471c1a356a8126d3
Joel Helbling <helblingjoel@gmail.com>
2022-09-17 00:53:28 +0100

    Added very basic smoketests

---
+++ dashboard/__tests__/smoke/pageloads.smoke.test.js
@@ -0,0 +1,68 @@
+const request = require("supertest");
+const app = require("../../app");
+
+describe("/", () => {
+  //   test("root", () => {
+  //     return request(app).get("/").expect(200);
+  //   });
+
+  describe("/energy", () => {
+    test("root", async () => {
+      await request(app).get("/energy").expect(200);
+    });
+    test("/bill", () => {
+      return request(app).get("/energy/bills").expect(200);
+    });
+    test("/insert_electric", () => {
+      return request(app).get("/energy/insert_electric").expect(200);
+    });
+    test("/insert_gas", () => {
+      return request(app).get("/energy/insert_gas").expect(200);
+    });
+    test("/view_monthly", () => {
+      return request(app).get("/energy/view_monthly").expect(200);
+    });
+    test("/view_hourly", () => {
+      return request(app).get("/energy/view_hourly").expect(200);
+    });
+  });
+
+  describe("/pokemon", () => {
+    test("/", () => {
+      return request(app).get("/pokemon").expect(200);
+    });
+    test("/blackwhite", () => {
+      return request(app).get("/pokemon/blackwhite").expect(200);
+    });
+    test("/search?search=ball", () => {
+      return request(app).get("/pokemon/search?search=ballch").expect(200);
+    });
+    test("/pkmn/1", () => {
+      return request(app).get("/pokemon/pkmn/1").expect(200);
+    });
+    test("/item/1", () => {
+      return request(app).get("/pokemon/item/1").expect(200);
+    });
+  });
+
+  describe("/stopwatch", () => {
+    test("/", () => {
+      return request(app).get("/stopwatch").expect(200);
+    });
+    test("/view", () => {
+      return request(app).get("/stopwatch/view").expect(200);
+    });
+  });
+
+  describe("/timesheet", () => {
+    test("/", () => {
+      return request(app).get("/timesheet").expect(200);
+    });
+    test("/view", () => {
+      return request(app).get("/timesheet/view").expect(200);
+    });
+    test("/edit", () => {
+      return request(app).get("/timesheet/edit").expect(200);
+    });
+  });
+});
