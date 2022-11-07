9e6d154189f084bc7d044809e62c24d8284ed01b
Joel Helbling <helblingjoel@gmail.com>
2022-11-07 21:35:37 +0000

    Include date

--- dashboard/src/utils/log.utils.ts
+++ dashboard/src/utils/log.utils.ts
@@ -2,6 +2,14 @@ import log from "loglevel";
 import prefix from "loglevel-plugin-prefix";
 import chalk from "chalk";
 
+const padWithLeadingCharacters = (
+  input: any,
+  desiredTotalLength: number,
+  character: string,
+): string => {
+  return String(input).padStart(desiredTotalLength, character);
+};
+
 prefix.reg(log);
 log.setDefaultLevel("TRACE");
 
@@ -14,6 +22,16 @@ const logColors = {
 };
 
 prefix.apply(log, {
+  timestampFormatter(date) {
+    return `${padWithLeadingCharacters(
+      date.getMonth() + 1,
+      2,
+      "0",
+    )}-${padWithLeadingCharacters(date.getDate(), 2, "0")} ${date.toLocaleTimeString(
+      "en-GB",
+    )}.${padWithLeadingCharacters(date.getMilliseconds(), 3, "0")}`;
+  },
+
   format(level, name, timestamp) {
     return `${chalk.grey("[" + timestamp + "]")} ${chalk.green(name)} ${logColors[
       level.toUpperCase()
