3465f106a1cc6dcf85e761734f3c39439bd178bc
Joel Helbling <helblingjoel@gmail.com>
2022-11-07 21:02:40 +0000

    Actually close connection

--- dashboard/src/db/index.ts
+++ dashboard/src/db/index.ts
@@ -4,7 +4,7 @@ class DB {
   static query = async (query: string, values?: Array<any>) => {
     const client = await DBSetup.getPool().connect();
     const result = await client.query(query, values);
-    client.release();
+    client.release(true);
     return result;
   };
 }
