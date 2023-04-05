c65a867c4778266fc10b8aac4af3f991244ae5a4
Joel Helbling <helblingjoel@gmail.com>
2023-04-06 00:00:54 +0100

    Clear out old files

--- dashboard/src/public/scripts/cardheadtoggle.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let cardToggles = document.getElementsByClassName("card-toggle");
-
-  for (let i = 0; i < cardToggles.length; i++) {
-    cardToggles[i].addEventListener("click", (e) => {
-      e.currentTarget.parentNode.childNodes[1].classList.toggle("is-hidden");
-    });
-  }
-});
