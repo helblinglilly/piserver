6898b27216e87f9685db6d6ba93f527d970e8a8f
Joel Helbling <helblingjoel@gmail.com>
2023-04-07 18:18:03 +0100

    Refactor into fn component

--- src/components/datePicker.tsx
+++ src/components/datePicker.tsx
@@ -1,38 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
-interface DatePickerState {
-	currentValue: Date;
-	hasChanged: boolean;
-}
-export default class DatePicker extends React.Component<
-	{
-		// eslint-disable-next-line no-unused-vars
-		changeHandler: (date: Date) => any;
-		name: string;
-		isReadOnly: boolean;
-		initialDate: Date;
-		minDate: Date;
-		maxDate: Date;
-	},
-	DatePickerState
-> {
-	constructor(props: {
-		// eslint-disable-next-line no-unused-vars
-		changeHandler: (date: Date) => any;
-		formName?: string;
-		isReadOnly: boolean;
-		initialDate: Date;
-		minDate: Date;
-		maxDate: Date;
-	}) {
-		super(props);
-		this.state = {
-			currentValue: this.props.initialDate,
-			hasChanged: false,
-		};
-	}
+export default function DatePicker(props: {
+	// eslint-disable-next-line no-unused-vars
+	changeHandler: (date: Date) => any;
+	name: string;
+	isReadOnly: boolean;
+	initialDate: Date;
+	minDate: Date;
+	maxDate: Date;
+}) {
+	const [currentValue, setCurrentValue] = useState(props.initialDate);
+	const [hasChanged, setHasChanged] = useState(false);
 
-	internalChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+	const internalChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const date = new Date(e.target.value);
 		try {
 			date.toISOString();
@@ -40,31 +20,28 @@ export default class DatePicker extends React.Component<
 			return;
 		}
 
-		this.setState({
-			currentValue: date,
-			hasChanged: true,
-		});
-		this.props.changeHandler(date);
+		setHasChanged(true);
+		setCurrentValue(date);
+
+		props.changeHandler(date);
 	};
 
-	render() {
-		return (
-			<input
-				className="input"
-				type="date"
-				id={`${this.props.name}-date`}
-				name={this.props.name ? this.props.name : ""}
-				onChange={this.internalChangeHandler}
-				min={this.props.minDate.toISOString().split("T")[0]}
-				max={this.props.maxDate.toISOString().split("T")[0]}
-				value={
-					this.state.hasChanged
-						? this.state.currentValue.toISOString().split("T")[0]
-						: this.props.initialDate.toISOString().split("T")[0]
-				}
-				disabled={this.props.isReadOnly}
-				required
-			/>
-		);
-	}
+	return (
+		<input
+			className="input"
+			type="date"
+			id={`${props.name}-date`}
+			name={props.name ? props.name : ""}
+			onChange={internalChangeHandler}
+			min={props.minDate.toISOString().split("T")[0]}
+			max={props.maxDate.toISOString().split("T")[0]}
+			value={
+				hasChanged
+					? currentValue.toISOString().split("T")[0]
+					: props.initialDate.toISOString().split("T")[0]
+			}
+			disabled={props.isReadOnly}
+			required
+		/>
+	);
 }
