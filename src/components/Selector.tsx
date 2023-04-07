095f14a8e4773828124d19275e44040623d6c0b3
Joel Helbling <helblingjoel@gmail.com>
2023-04-07 22:18:03 +0100

    Add custom styling to selector

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -1,13 +1,17 @@
+import { CSSProperties } from "react";
+
 export default function Selector(props: {
 	possibleValues: any[];
 	initialValue: any;
 	supplementary: string;
 	// eslint-disable-next-line no-unused-vars
 	onSelectHandler: (amount: any) => any;
+	className?: string;
+	style?: CSSProperties;
 }) {
 	return (
-		<div className="select is-normal">
-			<select defaultValue={props.initialValue}>
+		<div className={`select is-normal ${props.className}`} style={props.style}>
+			<select defaultValue={props.initialValue} style={props.style}>
 				{props.possibleValues.map((optionValue) => {
 					return (
 						<option
