054fc1a336d300ab27bdb421e9c6487fc1f1a152
Lilly Helbling <lilly@helbling.uk>
2024-11-02 14:04:49 +0000

    Remove server set up scripts
    This has been migrated to an Ansible project in the infrastructure repo

--- scripts/strawberry.sh
+++
@@ -1,8 +0,0 @@
-chmod +x ./install-docker.sh
-sudo ./install-docker.sh
-mv strawberry-docker-compose.yaml ~/docker-compose.yaml
-
-cd ~
-docker compose up -d
-
-(crontab -l ; echo "0 * * * * curl http://192.168.0.11/api/jobs/energy") | crontab -
